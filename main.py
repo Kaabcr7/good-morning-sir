ad670a8b0aa54e7cf2c93b536adff4bfe61b6287
Kaabcr7 <162888786+Kaabcr7@users.noreply.github.com>
2024-06-16 00:23:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import time
 timestamp = time.strftime('%H:%M:%S')
 print(timestamp)
 timestamp = int(time.strftime('%H'))
-# print(timestamp)
-# timestamp = time.strftime('%M')
-# print(timestamp)
-# timestamp = time.strftime('%S')
-# print(timestamp)
+
 if (4<=timestamp<12):
     print("Good morning sir")
 elif (12<timestamp<17):
